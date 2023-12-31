d377c20906cc970ad6f6887a97fc15388d442321
Mainque Bautista Menjoulou <mmenjoulou@alumnos.exa.unicen.edu.ar>
2023-12-31 16:01:26 -0300

    Agregadas las primeras rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TareasComponent } from './components/tareas/tareas.component';
+import { NewTareaComponent } from './components/new-tarea/new-tarea.component';
+import { EditTareaComponent } from './components/edit-tarea/edit-tarea.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch:'full'},
+  { path: 'login', component: LoginComponent},
+  { path: 'tareas', component: TareasComponent},
+  { path: 'new-tarea', component: NewTareaComponent},
+  { path: 'edit-tarea', component: EditTareaComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, TareasComponent, NewTareaComponent, EditTareaComponent];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { TareasComponent } from './components/tareas/tareas.component';
-import { NewTareaComponent } from './components/new-tarea/new-tarea.component';
-import { EditTareaComponent } from './components/edit-tarea/edit-tarea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    TareasComponent,
-    NewTareaComponent,
-    EditTareaComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
